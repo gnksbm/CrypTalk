f14c1ce073d151daccbfb373e9cbfdc2fbe405a7
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-08-15 13:27:51 +0900

    initial Commit

---
+++ Coin/Sources/Presentation/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Coin
+//
+//  Created by gnksbm on 8/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
