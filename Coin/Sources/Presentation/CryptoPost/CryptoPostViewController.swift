6b077b314bd137ffec3abc10f101cccfb620fdf6
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-08-29 01:43:59 +0900

    [Refactor] CryptoPostHeaderView 분리

--- Coin/Sources/Presentation/CryptoPost/CryptoPostViewController.swift
+++ Coin/Sources/Presentation/CryptoPost/CryptoPostViewController.swift
@@ -10,22 +10,16 @@ import UIKit
 import CoinFoundation
 import Domain
 
-import Kingfisher
 import RxSwift
 import SnapKit
 import Neat
 
 final class CryptoPostViewController: BaseViewController, ViewType {
-    private let iconImageView = UIImageView().nt.configure {
-        $0.contentMode(.scaleAspectFill)
+    private let headerView = CryptoPostHeaderView()
+    private lazy var tableView = UITableView().nt.configure {
+        $0.tableHeaderView(headerView)
+            .backgroundColor(.clear)
     }
-    private let titleButton = UIButton(configuration: .plain()).nt.configure {
-        $0.configuration.titleAlignment(.leading)
-            .configuration.baseForegroundColor(Design.Color.foregroundColor)
-    }
-    private let priceLabel = UILabel()
-    private let rateLabel = UILabel()
-    private let tableView = UITableView()
     private let pageChangeEvent = BehaviorSubject(value: 1)
     
     init(viewModel: CryptoPostViewModel) {
@@ -46,67 +40,22 @@ final class CryptoPostViewController: BaseViewController, ViewType {
             output.cryptoCurrency
                 .withUnretained(self)
                 .subscribe { vc, response in
-                    vc.updateView(response: response)
+                    vc.headerView.updateView(response: response)
                 }
             
             output.cryptoPostResponse
                 .withUnretained(self)
                 .subscribe { vc, responses in
-                    vc.updateView(responses: responses)
+                    vc.headerView.updateView(responses: responses)
                 }
         }
     }
     
-    private func updateView(response: CryptoCurrencyResponse) {
-        iconImageView.kf.setImage(with: response.imageURL)
-        titleButton.configuration?.title = response.name
-        priceLabel.text = response.price.formatted(.currency(code: "KRW"))
-        rateLabel.text = response.rate.formatted(.percent)
-    }
-    
-    private func updateView(responses: [PostResponse]) {
-        print(responses.count)
-    }
-    
     override func configureLayout() {
-        [
-            iconImageView,
-            titleButton,
-            priceLabel,
-            rateLabel,
-            tableView
-        ].forEach { view.addSubview($0) }
-        
-        iconImageView.snp.makeConstraints { make in
-            make.top.leading.equalTo(safeArea)
-                .inset(Design.Padding.regular)
-            make.size.equalTo(Design.Dimension.symbolSize)
-        }
-        
-        titleButton.snp.makeConstraints { make in
-            make.centerY.equalTo(iconImageView)
-            make.leading.equalTo(iconImageView.snp.trailing)
-                .offset(Design.Padding.regular)
-        }
-        
-        priceLabel.snp.makeConstraints { make in
-            make.centerY.equalTo(titleButton)
-            make.leading.equalTo(titleButton.snp.trailing)
-                .offset(Design.Padding.regular)
-            make.trailing.equalTo(safeArea)
-                .inset(Design.Padding.regular)
-        }
-        
-        rateLabel.snp.makeConstraints { make in
-            make.top.equalTo(priceLabel.snp.bottom)
-                .offset(Design.Padding.regular)
-            make.trailing.equalTo(priceLabel)
-        }
+        [tableView].forEach { view.addSubview($0) }
         
         tableView.snp.makeConstraints { make in
-            make.top.equalTo(rateLabel.snp.bottom)
-                .offset(Design.Padding.regular)
-            make.horizontalEdges.bottom.equalTo(safeArea)
+            make.edges.equalTo(safeArea)
         }
     }
 }
---
+++ Coin/Sources/Presentation/CryptoPost/View/CryptoPostHeaderView.swift
@@ -0,0 +1,91 @@
+//
+//  CryptoPostHeaderView.swift
+//  Coin
+//
+//  Created by gnksbm on 8/29/24.
+//
+
+import UIKit
+
+import CoinFoundation
+import Domain
+
+import Kingfisher
+import Neat
+
+final class CryptoPostHeaderView: BaseView {
+    private let iconImageView = UIImageView().nt.configure {
+        $0.contentMode(.scaleAspectFill)
+    }
+    private let titleButton = UIButton(configuration: .plain()).nt.configure {
+        $0.configuration.titleAlignment(.leading)
+            .configuration.baseForegroundColor(Design.Color.foregroundColor)
+            .setContentCompressionResistancePriority(
+                .required,
+                for: .horizontal
+            )
+    }
+    private let priceLabel = UILabel().nt.configure {
+        $0.textColor(Design.Color.foregroundColor)
+            .textAlignment(.right)
+    }
+    private let rateLabel = UILabel().nt.configure {
+        $0.textColor(Design.Color.foregroundColor)
+            .textAlignment(.right)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let size = systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        self.frame.size.height = size.height
+    }
+    
+    func updateView(response: CryptoCurrencyResponse) {
+        iconImageView.kf.setImage(with: response.imageURL)
+        titleButton.configuration?.title = response.name
+        priceLabel.text = response.price.formatted(.currency(code: "KRW"))
+        rateLabel.text = response.rate.formatted(.percent)
+    }
+    
+    func updateView(responses: [PostResponse]) {
+        print(responses.count)
+    }
+    
+    override func configureLayout() {
+        [
+            iconImageView,
+            titleButton,
+            priceLabel,
+            rateLabel
+        ].forEach { addSubview($0) }
+        
+        iconImageView.snp.makeConstraints { make in
+            make.top.leading.equalTo(self)
+                .inset(Design.Padding.regular).priority(.required)
+            make.size.equalTo(Design.Dimension.symbolSize).priority(.required)
+        }
+        
+        titleButton.snp.makeConstraints { make in
+            make.centerY.equalTo(iconImageView)
+            make.leading.equalTo(iconImageView.snp.trailing)
+                .offset(Design.Padding.regular)
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(titleButton)
+            make.leading.equalTo(titleButton.snp.trailing)
+                .offset(Design.Padding.regular).priority(.required)
+            make.trailing.equalTo(self)
+                .inset(Design.Padding.regular).priority(.required)
+        }
+        
+        rateLabel.snp.makeConstraints { make in
+            make.top.equalTo(priceLabel.snp.bottom)
+                .offset(Design.Padding.regular)
+            make.trailing.equalTo(priceLabel)
+            make.bottom.equalTo(self)
+                .inset(Design.Padding.regular).priority(.required)
+        }
+    }
+}
